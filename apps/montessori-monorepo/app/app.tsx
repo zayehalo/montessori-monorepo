4dbb55e4843a4ef60daf17af9528f0f095b65e40
Crystal Dennis <zayehalo@gmail.com>
2025-09-16 21:01:34 -0400

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/8AvbMqzyNL

---
+++ apps/montessori-monorepo/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@montessori-monorepo/montessori-monorepo" />
+    </div>
+  );
+}
+
+export default App;
